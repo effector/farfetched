434462ad449b36f6559bda8fec3d14e1ca89654f
Igor Kamyshev <igor@kamyshev.me>
2025-09-28 12:39:25 +0700

    Delete code

--- packages/core/src/query/__tests__/query.started.test.ts
+++ packages/core/src/query/__tests__/query.started.test.ts
@@ -1,11 +1,9 @@
 import { allSettled, createWatch, fork } from 'effector';
 import { describe, test, expect, vi } from 'vitest';
-import { attachOperation } from '../../attach/attach';
 
 import { unknownContract } from '../../contract/unknown_contract';
 import { fetchFx } from '../../fetch/fetch';
 import { createJsonQuery } from '../create_json_query';
-import { createQuery } from '../create_query';
 
 describe('Query#started', () => {
   test('should be fired after start', async () => {
@@ -33,29 +31,5 @@ describe('Query#started', () => {
     // Should not start Query again
     await allSettled(query.refresh, { scope });
     expect(startedListener).toBeCalledTimes(1);
-  });
-
-  test('support attachOperation, issue #305', async () => {
-    const original = createQuery({ handler: vi.fn() });
-    const attached = attachOperation(original);
-
-    const originalStartedListener = vi.fn();
-
-    const scope = fork();
-
-    createWatch({ unit: original.started, fn: originalStartedListener, scope });
-
-    await allSettled(attached.start, { scope, params: 100 });
-
-    expect(originalStartedListener).toBeCalledTimes(1);
-    expect(originalStartedListener).toBeCalledWith(
-      expect.objectContaining({
-        params: 100,
-        meta: {
-          stale: true,
-          stopErrorPropagation: false,
-        },
-      })
-    );
   });
 });
